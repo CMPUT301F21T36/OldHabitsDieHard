c0d16ec431368f4758e49dc1d0e834410bf219b9
Rowan <rtilroe@ualberta.ca>
2021-11-05 00:27:23 -0600

    Updated user tests

--- app/src/test/java/com/example/oldhabitsdiehard/UserTest.java
+++ app/src/test/java/com/example/oldhabitsdiehard/UserTest.java
@@ -1,12 +1,15 @@
 package com.example.oldhabitsdiehard;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Tests for the User class
@@ -46,11 +49,17 @@ public class UserTest {
     }
 
     @Test
-    void testGetHabits() {
+    void testHabitsHandling() {
         User user = mockUser();
         Habit habit = new Habit("title", "reason");
+
+        // Adding and getting
         user.addHabit(habit);
         assertTrue(user.getHabits().contains(habit));
+
+        // Deleting and getting
+        user.deleteHabit(habit);
+        assertFalse(user.getHabits().contains(habit));
     }
 
     @Test
@@ -78,12 +87,37 @@ public class UserTest {
 
     @Test
     void testTodayHabits() {
-        boolean[] weekdays = {true, true, true, true, true, true, true};
+        ArrayList<Boolean> weekdays = new ArrayList<Boolean>();
+        for (int i = 0; i<7; i++) {
+            weekdays.add(true);
+        }
+
         Habit habit = new Habit("", "", weekdays);
         User user = mockUser();
         user.addHabit(habit);
+
         ArrayList<Habit> todayHabits = user.getTodayHabits();
         assertTrue(todayHabits.contains(habit));
     }
 
+    @Test
+    void testIllegalArguments() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            new User("", "");
+        });
+    }
+
+    @Test
+    void testHabitEventHandling() {
+        User user = mockUser();
+        Habit habit = new Habit("title", "reason");
+        user.addHabit(habit);
+
+        // Adding
+        HabitEvent event = new HabitEvent("title","comment",LocalDate.of(2021, 11, 4));
+        user.addHabitEvent(event);
+
+        // Getting
+        assertTrue(user.getHabitEvents().contains(event));
+    }
 }
