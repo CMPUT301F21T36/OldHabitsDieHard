039572a1f494a7a848c1676f4c8f79c0cbff3437
Chanpreet Singh <ch12@ualberta.ca>
2021-11-21 18:07:12 -0700

    input rules

--- app/src/main/java/com/example/oldhabitsdiehard/HabitFragment.java
+++ app/src/main/java/com/example/oldhabitsdiehard/HabitFragment.java
@@ -224,7 +224,7 @@ public class HabitFragment extends DialogFragment {
                     }).create();
         } else {
             // we are adding a habit
-            return builder
+            builder
                     .setView(view)
                     .setTitle("Add Habit")
                     .setNegativeButton("Cancel", null)
@@ -237,7 +237,19 @@ public class HabitFragment extends DialogFragment {
                         @RequiresApi(api = Build.VERSION_CODES.O)
                         @Override
                         public void onClick(DialogInterface dialogInterface, int i) {
-                            // get info that the user entered
+
+                        }
+                    });
+
+
+                    AlertDialog dialog = builder.create();
+                    dialog.show();
+                    //Overriding the handler immediately after show is probably a better approach than OnShowListener as described below
+                    dialog.getButton(AlertDialog.BUTTON_POSITIVE).setOnClickListener(new View.OnClickListener()
+                    {
+                        @Override
+                        public void onClick(View v)
+                        {
                             String title = habitTitle.getText().toString();
                             String reason = habitReason.getText().toString();
 
@@ -269,10 +281,18 @@ public class HabitFragment extends DialogFragment {
                             weekdays.add(friday.isChecked());
                             weekdays.add(saturday.isChecked());
 
-                            // add habit to listener
-                            listener.addHabit(new Habit(title, reason, date, weekdays, isPublic));
-                        }
-                    }).create();
+                            Boolean wantToCloseDialog = false;
+                            if(habitTitle.length()<=0){
+                                habitTitle.setError("Habit Title Required");
+                            }else{
+                                listener.addHabit(new Habit(title, reason, date, weekdays, isPublic));
+                                wantToCloseDialog = true;
+                            }
+                            if(wantToCloseDialog)
+                                dialog.dismiss();
+                            }
+                    });
+                    return dialog;
         }
     }
 }
